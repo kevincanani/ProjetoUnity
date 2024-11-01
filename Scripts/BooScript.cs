e28467bf47c566cde52721fd3ba8867849d708c7
Níckolas <nickolasgodimsantos@gmail.com>
2024-11-01 17:46:06 -0300

    Add files via upload

---
+++ Scripts/BooScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooScript : MonoBehaviour
+{
+    private GameObject Player;
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player")
+
+        [SerializeField] private float velocidadeDoInimigo;
+
+        [SerializeField] private bool indoParaDireita;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SeguirJogador()
+}
